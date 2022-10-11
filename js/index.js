72c95e0869faebaf947e70619fd45d4e1b65723a
mkrvi <mkrv.iryna@gmail.com>
2022-10-11 17:31:09 +0300

    ready hw

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const firstNumber = +prompt('Enter the first number');
+
+const secondNumber = +prompt('Enter the second number');
+
+const thirdNumber = +prompt('Enter the third number');
+
+const result = (firstNumber + secondNumber + thirdNumber)/3;
+
+alert(result);
